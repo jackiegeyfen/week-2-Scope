fec998b2cdab7ffaffc61a94df16a53647b668c5
Chanary Wilson <chanarykw@gmail.com>
2023-08-16 21:39:44 -0700

    04 arrow done

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -3,14 +3,21 @@ Write a function using fat arrow syntax, `arrowAvgValue(array)` that takes in an
 array of numbers and returns the average number. Assign the below function to a
 variable using the const keyword.
 
-Examples:
+
+***********************************************************************/
+
+const arrowAvgValue = (array) => {
+  let sum = 0;
+
+  array.forEach((num) => {
+    sum += num;
+  })
+  console.log(sum / array.length);
+};
 
 arrowAvgValue([10, 20]); // => 15
 arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
-***********************************************************************/
-
-// Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
