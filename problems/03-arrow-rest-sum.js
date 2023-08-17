e48bbcb93312340a63e8d41c5cb985da19b429b6
Jacqueline <jgeyfen@gmail.com>
2023-08-16 21:35:55 -0700

    03 done

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,7 +11,13 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here
+const arrowRestSum = (...args) => {
+  let Sum=0;
+  args.forEach((num) => {
+    Sum += num;
+  })
+  return Sum;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
