424b65706f8d409bff5cc9f44d5b857ef28c0d31
Jacqueline <jgeyfen@gmail.com>
2023-08-17 20:03:58 -0700

    interrupter done

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -4,9 +4,6 @@ accept a word and return a function. When the function returned by interrupter
 is invoked with a string the string will be returned with "interruptions".
 
 Look below to see how this function is invoked:
-let rudePerson = interrupter("what"); // => returns a function
-console.log(rudePerson("how are you")); // prints "how what are what you"
-console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
 Invoking the interrupter function again:
@@ -16,8 +13,19 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 ***********************************************************************/
 
-// Your code here
+// const interrupter= (interruptingWord) => (string) => string.split(' ').join(" " + interruptingWord + " ")
 
+const interrupter= (interruptingWord) => {
+  let newInterrupter=(string)=>{
+    let array=string.split(' ');
+    return array.join(" " + interruptingWord + " ");
+  }
+  return newInterrupter;
+}
+
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
