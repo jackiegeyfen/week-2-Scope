564b5e4b88a05bd8bc3365a05c9da925688cc43e
Chanary Wilson <chanarykw@gmail.com>
2023-08-17 19:21:11 -0700

    06 closure sando

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -4,20 +4,24 @@ This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
-Look below to see how this function is invoked:
+***********************************************************************/
+
+const sandwichMaker = () => {
+  let str = "One sandwich with tomato";
+  let newFunction = (ingredient) => {
+    console.log(str += " and " + ingredient);
+  }
+  return newFunction;
+};
+
 
 let sandwich = sandwichMaker(); // => returns a function
 sandwich("spinach") // => "One sandwich with tomato and spinach"
 sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
 sandwich("bread") // => "One sandwich with tomato and spinach and jelly and bread"
 
-Another Sandwich:
 let sandwich2 = sandwichMaker(); // => returns a function
 sandwich2("pb") // => "One sandwich with tomato and pb"
-
-***********************************************************************/
-
-// Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
